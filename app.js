7b83954c6cdc2706cf8c5d91b3b185ccc6ef0400
yu <112287812@qq.com>
2018-07-12 15:32:11 +0800

    change env to enp

--- app.js
+++ app.js
@@ -7,9 +7,9 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 const pollutionMonitor = require('./routes/pollutionMonitor');
 const sentimentAnalysis = require('./routes/sentimentAnalysis');
-const letters = require('./routes/env/poa/letters');
-const common = require('./routes/env/poa/common');
-const news = require('./routes/env/poa/news');
+const letters = require('./routes/enp/poa/letters');
+const common = require('./routes/enp/poa/common');
+const news = require('./routes/enp/poa/news');
 
 var app = express();
 
@@ -34,9 +34,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/pollutionMonitor', pollutionMonitor);
 app.use('/sentimentAnalysis', sentimentAnalysis);
-app.use('/env/poa/letters', letters);
-app.use('/env/poa/common', common);
-app.use('/env/poa/news', news);
+app.use('/enp/poa/letters', letters);
+app.use('/enp/poa/common', common);
+app.use('/enp/poa/news', news);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
